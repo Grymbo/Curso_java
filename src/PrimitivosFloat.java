2bdbea582e356ba79f04626a7410fab9199f07b1
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-01-23 07:24:05 -0600

    Primitivos update

--- src/PrimitivosFloat.java
+++ src/PrimitivosFloat.java
@@ -2,5 +2,16 @@ public class PrimitivosFloat {
     public static void main(String[] args) {
         float realFloat = 2120f;
         System.out.println("realFloat = " + realFloat);
+        System.out.println("float corresponde en byte a = " + Float.BYTES);
+        System.out.println("float corresponde en bIteS a = " + Float.SIZE);
+        System.out.println("el valor maximo para float es de = " + Float.MAX_VALUE);
+        System.out.println("el valor minimo para float es de = " + Float.MIN_VALUE);
+
+        double realDouble = 3.4028235E39;
+        System.out.println("realDouble = " + realDouble);
+        System.out.println("double corresponde en byte a = " + Double.BYTES);
+        System.out.println("double corresponde en bIteS a = " + Double.SIZE);
+        System.out.println("el valor maximo para double es de = " + Double.MAX_VALUE);
+        System.out.println("el valor minimo para double es de = " + Double.MIN_VALUE);
     }
 }
